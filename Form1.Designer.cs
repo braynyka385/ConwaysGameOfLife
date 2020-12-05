353233b0cac47bac8de36042fa11fa82bddb31e0
Limitlez26 <syntaxerror212@gmail.com>
2020-12-04 21:59:40 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace ConwaysGameOfLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.runButton = new System.Windows.Forms.Button();
+            this.debugLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(713, 214);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(862, 93);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 1;
+            this.runButton.Text = "--->";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // debugLabel
+            // 
+            this.debugLabel.AutoSize = true;
+            this.debugLabel.ForeColor = System.Drawing.Color.White;
+            this.debugLabel.Location = new System.Drawing.Point(849, 289);
+            this.debugLabel.Name = "debugLabel";
+            this.debugLabel.Size = new System.Drawing.Size(35, 13);
+            this.debugLabel.TabIndex = 2;
+            this.debugLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1002, 760);
+            this.Controls.Add(this.debugLabel);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Label debugLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConwaysGameOfLife
+{
+    public partial class Form1 : Form
+    {
+        Button pressedGridButton;
+        Button pressedLifeButton;
+        int scaleValue = 60;
+        int[] lifeLocations = new int[200];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false;
+
+            for (int x = 3; x <= 11; x++)
+            {
+                for (int y = 3; y <= 11; y++)
+                {
+                    Button gridButton = new Button();
+                    this.Controls.Add(gridButton);
+                    gridButton.Location = new Point(x * scaleValue, y * scaleValue);
+                    gridButton.Size = new Size(scaleValue,scaleValue);
+                    gridButton.Click += new EventHandler(gridButton_Click);
+                }
+            }
+        }
+        public void gridButton_Click (object sender, EventArgs e)
+        {
+            pressedGridButton = (Button)sender;
+            debugLabel.Text = pressedGridButton.Location.X / scaleValue + "\n" + pressedGridButton.Location.Y / scaleValue;
+            Button lifeButton = new Button();
+            this.Controls.Add(lifeButton);
+            lifeButton.Size = pressedGridButton.Size;
+            lifeButton.Location = pressedGridButton.Location;
+            lifeButton.BringToFront();
+            lifeButton.BackColor = Color.White;
+            lifeButton.Click += new EventHandler(lifeButton_Click);
+
+            for (int x = 1; x <= 10; x++)
+            {
+                for (int y = 1; y <= 10; y++)
+                {
+                    if (x * scaleValue == lifeButton.Location.X && y * scaleValue == lifeButton.Location.Y)
+                    {
+                        lifeLocations[y * 10 + x - 1] = 1;
+                    }
+                }
+            }
+        }
+
+        public void lifeButton_Click (object sender, EventArgs e)
+        {
+            pressedLifeButton = (Button)sender;
+            for (int x = 1; x <= 10; x++)
+            {
+                for (int y = 1; y <= 10; y++)
+                {
+                    if (x * scaleValue == pressedLifeButton.Location.X && y * scaleValue == pressedLifeButton.Location.Y)
+                    {
+                        lifeLocations[y * 10 + x - 1] = 0;
+                    }
+                }
+            }
+            pressedLifeButton.Visible = false;
+
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            for (int x = 1; x <= 99; x++)
+            {
+                if (lifeLocations[x] == 1)
+                {
+                    int localLife = 0;
+                    if (lifeLocations[x - 1] == 1)
+                    {
+                        localLife++;
+                    }
+                    if (lifeLocations[x + 1] == 1)
+                    {
+                        localLife++;
+                    }
+                    if (lifeLocations[x + 10] == 1)
+                    {
+                        localLife++;
+                    }
+                    if (lifeLocations[x - 10] == 1)
+                    {
+                        localLife++;
+                    }
+                    if (lifeLocations[x + 11] == 1)
+                    {
+                        localLife++;
+                    }
+                    if (lifeLocations[x - 11] == 1)
+                    {
+                        localLife++;
+                    }
+                    if (lifeLocations[x + 9] == 1)
+                    {
+                        localLife++;
+                    }
+                    if (lifeLocations[x - 9] == 1)
+                    {
+                        localLife++;
+                    }
+
+                    if (localLife < 2 || localLife > 3)
+                    {
+                        lifeLocations[x] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
